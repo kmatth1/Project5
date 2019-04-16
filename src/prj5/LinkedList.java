6500538284c3747b36c936e2e6b7f19123e6c072
kmatth1 <kmatth1@vt.edu>
2019-04-16 15:26:37 -0400

    Create LinkedList.java

---
+++ src/prj5/LinkedList.java
@@ -0,0 +1,25 @@
+package prj5;
+/**
+ * Reads and holds data from two input files. One
+ * file holds survey information about students and 
+ * the music they listen to. Another holds information
+ * about the songs in the same survey.
+ *
+ * @author Matthew Krivansky (kmatth1)
+ * @version 04/15/2019
+ */
+public class LinkedList 
+{
+    private T head;
+    private Node<T> nextNode;
+    
+    /**
+     * LinkedList constructor
+     * @param <T>
+     */
+    public LinkedList<T>()
+    {
+        
+    }
+    
+}
